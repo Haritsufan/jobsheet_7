69a23e2cb383eb8c67ead57712f8fecf14013d24
= <=>
2023-06-18 12:30:34 +0700

    first commit

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,66 @@
+@extends('mahasiswa.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left mt-2">
+                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
+            </div>
+            <form action="{{ route('mahasiswa.index') }}" method="GET" role="search">
+                <div class="input-group">
+                    <input type="text" class="form-control" name="search" placeholder="Cari nama mahasiswa">
+                    <span class="input-group-btn">
+                        <button class="btn btn-secondary" type="submit">Cari</button>
+                    </span>
+                </div>
+            </form>
+
+            <div class="float-right my-2">
+                <a class="btn btn
+    success" href="{{ route('mahasiswa.create') }}"> Input Mahasiswa</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>Nim</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Jurusan</th>
+            <th>No_Handphone</th>
+            <th>Email</th>
+            <th>Tanggal_Lahir</th>
+
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($mahasiswas as $Mahasiswa)
+            <tr>
+
+                <td>{{ $Mahasiswa->Nim }}</td>
+                <td>{{ $Mahasiswa->Nama }}</td>
+                <td>{{ $Mahasiswa->Kelas }}</td>
+                <td>{{ $Mahasiswa->Jurusan }}</td>
+                <td>{{ $Mahasiswa->No_Handphone }}</td>
+                <td>{{ $Mahasiswa->Email }}</td>
+                <td>{{ $Mahasiswa->Tanggal_Lahir }}</td>
+
+                <td>
+                    <form action="{{ route('mahasiswa.destroy', $Mahasiswa->Nim) }}" method="POST">
+
+                        <a class="btn btninfo" href="{{ route('mahasiswa.show', $Mahasiswa->Nim) }}">Show</a>
+                        <a class="btn btnprimary" href="{{ route('mahasiswa.edit', $Mahasiswa->Nim) }}">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
